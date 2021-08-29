cf2600b0f16cef1562e239e89adf8aaea07b5221
gnwncpta <gnwncpta@gmail.com>
2021-08-29 19:52:01 +0700

    add: tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+    zIndex: {
+      '1': '1',
+      '2': '2',
+      '3': '3',
+      '4': '4',
+      '5': '5'
+    }
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
